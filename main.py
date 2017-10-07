4cefaeca5437e42795429d20996a6a06dd07e01c
dhruvramani <dhruvramani98@gmail.com>
2017-10-07 18:19:43 +0530

    Coding Done

--- main.py
+++ main.py
@@ -7,21 +7,29 @@ from keras.layers import Dense, Dropout
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 num_epoch, batch_size = 100, 50
-input_dim, random_dim, output_dim = 1001, 100, 53
+input_dim, random_dim, output_dim = 500, 100, 983
 x = (Input(shape = (input_dim, ), dtype='float32'))
 noise = (Input(shape=(random_dim, ), dtype='float32'))
 y = (Input(shape = (output_dim, ), dtype='float32'))
 
+def get_data(path, noise=False):
+    data = np.load(path)
+    if noise == True :
+        data = data + np.random.normal(0, 0.001, data.shape)
+    return data
+
 def generator_model():
     start = concatenate([noise, x])
-    layer1 = Dense(680, input_dim = input_dim + random_dim, activation='relu')(start)
+    layer1 = Dense(350, input_dim = input_dim + random_dim, activation='relu')(start)
     layer1 = Dropout(0.5)(layer1)
-    layer2 = Dense(350, activation='relu')(layer1)
+    layer2 = Dense(120, activation='relu')(layer1)
     layer2 = Dropout(0.5)(layer2)
-    layer3 = Dense(100, activation='relu')(layer2)
+    layer3 = Dense(460, activation='relu')(layer2)
     layer3 = Dropout(0.5)(layer3)
-    layer4 = Dense(output_dim, activation='sigmoid')(layer3)
-    model = Model(input=[noise, x], output=layer4)
+    layer4 = Dense(780, activation='relu')(layer3)
+    layer4 = Dropout(0.5)(layer4)
+    layer5 = Dense(output_dim, activation='sigmoid')(layer4)
+    model = Model(input=[noise, x], output=layer5)
     return model
 
 def discriminator_model(): # Concatenated
@@ -43,7 +51,7 @@ def generator_containing_discriminator(generator, discriminator):
     model = Model(input=[x, noise], output=disc_val)
 
 def train():
-    # TODO : Data I/O
+    X_train, Y_train = get_data('./data/delicious/delicious-train-features.pkl'), get_data('./data/delicious/delicious-train-labels.pkl')
     discriminator = discriminator_model()
     generator = generator_model()
     disc_on_gen = generator_containing_discriminator(generator, discriminator)
@@ -62,6 +70,29 @@ def train():
             for i in range(batch_size):
                 noise[i, :] = np.random.uniform(-1, 1, random_dim)
             x_batch, y_batch = X_train[index * batch_size : (index + 1) * batch_size], Y_train[index * batch_size : (index + 1) * batch_size]
-            fake_y = generator.predict([x_batch])
+            fake_y = generator.predict([x_batch, noise], verbose=0)
+            real_pairs = np.concatenate((x_batch, y_batch), axis=1)
+            fake_pairs = np.concatenate((x_batch, fake_y), axis=1)
+            x = np.concatenate((real_pairs, fake_pairs))
+            y = np.asarray([1] * batch_size + [0] * batch_size, dtype=np.float32)
+            d_loss = discriminator.train_on_batch(x, y)
+            print("Batch {} -  Disc. Loss : {} ".format(index, d_loss), end="")
+            discriminator.trainable = False
+            g_loss = disc_on_gen.train_on_batch([x_batch, noise], np.asarray([1] * batch_size, dtype=np.float32))
+            discriminator.trainable = True
+            print(" Gen. Loss : {}".format(index, d_loss), end="\r")
+            
+            if index % 10 == 9:
+                generator.save_weights('generator', True)
+                discriminator.save_weights('discriminator', True)
 
+def test(data_type='test'):
+    X, Y = get_data('./data/delicious/delicious-{}-features.pkl'.format(data_type)), get_data('./data/delicious/delicious-{}-labels.pkl'.format(data_type))
+    generator = generator_model()
+    generator.compile(loss='binary_crossentropy', optimizer="SGD")
+    generator.load_weights('generator')
+    gen_y = generator.predict(X)
 
+if __name__ == '__main__':
+    train()
+    test()
