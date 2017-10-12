efd30d041a648ffdf281dbb03f06b56aef7c1ac1
dhruvramani <dhruvramani98@gmail.com>
2017-10-12 21:53:34 +0530

    Pls work

--- fnn.py
+++ fnn.py
@@ -9,7 +9,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def get_data(path, noise=False):
     data = np.load(path)
-    if noise == True :
+    if noise == True :p
         data = data + np.random.normal(0, 0.001, data.shape)
     return data
 
@@ -20,7 +20,7 @@ def patk(y_true, y_pred):
        score = tf.identity(score)
    return score
 
-num_epoch, batch_size = 75, 50
+num_epoch, batch_size = 45, 50
 input_dim, random_dim, output_dim = 500, 100, 983
 model = Sequential()
 model.add(Dense(600, input_dim=input_dim, activation='relu'))
@@ -31,9 +31,12 @@ model.compile(loss = 'binary_crossentropy', optimizer='sgd', metrics=['accuracy'
 X_train, X_test, Y_train, Y_test = get_data("./data/delicious/delicious-train-features.pkl"), get_data("./data/delicious/delicious-test-features.pkl"), get_data("./data/delicious/delicious-train-labels.pkl"), get_data("./data/delicious/delicious-test-labels.pkl")
 model.fit(X_train, Y_train, epochs=num_epoch, batch_size = batch_size)
 loss_and_metrics = model.evaluate(X_test, Y_test, batch_size = batch_size)
-
+predictions = model.predict(X_test)
+print(eval_performance.patk(Y_test, predictions))
+'''
 num_batches = int(X_train.shape[0] / batch_size)
 for index in range(num_batches):
     x_batch, y_batch = X_test[index * batch_size :(index+1) * batch_size, : ], Y_test[index * batch_size : (index + 1), :]
     predictions = model.predict_on_batch(x_batch)
     print(eval_performance.evaluate(y_batch, predictions))
+'''
